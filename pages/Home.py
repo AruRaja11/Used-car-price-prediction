fad7b8d668f968a0881e2a8117e82752d1304169
Arun Raja <147836952+AruRaja11@users.noreply.github.com>
2025-04-11 13:23:27 +0530

    Add files via upload

---
+++ pages/Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Home', page_icon='🏠')
+
+st.title("Home")
+
+st.markdown("""<h2><u>Project Title:</u> CarDekho Used Car Price Prediction<h2>""", unsafe_allow_html=True)
+st.markdown("""<h4>Problem Statement:</h4>""", unsafe_allow_html=True)
+st.markdown("""<h4>Objective:</h4>""", unsafe_allow_html=True)
+st.markdown("""<p>Imagine you are working as a data scientist in Car Dheko, your aim is to enhance the customer experience and streamline the pricing process by leveraging machine learning. You need to create an accurate and user-friendly streamlit tool that predicts the prices of used cars based on various features. This tool should be deployed as an interactive web application for both customers and sales representatives to use seamlessly.</p>""", unsafe_allow_html=True)
+
+st.write("---")
+
+st.markdown("""<h4>Project Scope:</h4>""", unsafe_allow_html=True)
+st.markdown("""<p>We have historical data on used car prices from CarDekho, including various features such as make, model, year, fuel type, transmission type, and other relevant attributes from different cities. Your task as a data scientist is to develop a machine learning model that can accurately predict the prices of used cars based on these features. The model should be integrated into a Streamlit-based web application to allow users to input car details and receive an estimated price instantly.</P""", unsafe_allow_html=True)
+
+st.write('---')
+
